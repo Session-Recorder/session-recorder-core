711c79c4fa54d509d70413a8cc75dc0c38e36686
Sebastian Jeong <sebastianrcnt@gmail.com>
2021-02-25 21:06:28 +0900

    [feat] organize imports

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+// routers
 app.use("/", indexRouter);
 
 // catch 404 and forward to error handler
